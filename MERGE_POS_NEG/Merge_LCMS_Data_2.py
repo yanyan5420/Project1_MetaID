edbebd29ef5d42b8253ab5b2c71213d9944a5cbc
yy5420 <y.yan20@imperial.ac.uk>
2020-11-30 03:00:10 +0000

    update codes of descriptors and merge

---
+++ MERGE_POS_NEG/Merge_LCMS_Data_2.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import library to read Excel files
+import pandas as pd
+import functools, time
+
+
+def merge_data(intensity_file, sample_file, feature_file, output_file, 
+               filt_feature_columns, feature_name_column, sample_ID_column):
+    """
+    This function is to merge the intensity, sample, and feature files into one single file.
+
+    Parameters 
+    ==========
+    intensity_file: str
+        intensity file name.
+
+    sample_file: str
+        sample file name.
+
+    feature_file: str
+        feature file name.
+
+    output_file: str
+        the desired output file name.
+
+    filt_feature_columns: list of str
+        the needed feature column names
+
+    feature_name_column: str
+        the name of column containing feature names
+
+    sample_ID_column: str
+        the name of column containing sample ID
+    """
+    
+    # read input files
+    intensity = pd.read_excel(intensity_file, header=None)
+    sample = pd.read_excel(sample_file)
+    feature = pd.read_excel(feature_file)
+    
+    # filter feature columns
+    filt_feature = feature.loc[:,filt_feature_columns]
+    filt_feature = filt_feature.set_index(feature_name_column).T
+    
+    # combine intensity and sample ID
+    intensity.set_index(sample[sample_ID_column], inplace=True)
+    intensity.columns = filt_feature.columns
+    
+    # merge intensity with sample ID and filtered features
+    df = filt_feature.append(intensity)
+    df = df.rename_axis(None, axis = 1)
+    
+    # write to a Excel file
+    df.to_excel(output_file)
+
+
+# positive mode
+intensity_file = "LC-MS_data/Airwave1xcms_SLPOS_scaled_Data.xlsx"
+sample_file = "LC-MS_data/Airwave1xcms_SLPOS_scaled_SampleInfo.xlsx"
+feature_file = "LC-MS_data/SLPOS_MZRTstring_Airwave1_allBatches.xlsx"
+output_file = "LC-MS_data/POS_Data.xlsx"
+filt_feature_columns = ["MZRT_str", "InitialVarNr", "rtmed", "mzmed", "fimed"]
+feature_name_column = "MZRT_str"
+sample_ID_column = "ICID"
+
+merge_data(intensity_file, sample_file, feature_file, output_file, 
+           filt_feature_columns, feature_name_column, sample_ID_column)
+
+
+# negative mode
+intensity_file = "LC-MS_data/Airwave1xcms_SLNEG_scaled_Data.xlsx"
+sample_file = "LC-MS_data/Airwave1xcms_SLNEG_scaled_SampleInfo.xlsx"
+feature_file = "LC-MS_data/SLNEG_MZRTstring_Airwave1_allBatches.xlsx"
+output_file = "LC-MS_data/NEG_Data.xlsx"
+filt_feature_columns = ["MZRT_str", "InitialVarNr", "rtmed", "mzmed", "fimed"]
+feature_name_column = "MZRT_str"
+sample_ID_column = "ICID"
+
+merge_data(intensity_file, sample_file, feature_file, output_file,
+           filt_feature_columns, feature_name_column, sample_ID_column)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h1>Identification of metabolites in LC-MS untargeted datasets using machine learning</h1>
 
-There are three folders: <strong>DataPreprocessing</strong>, <strong>DataVisualization</strong> and <strong>Figure</strong>.
+There are five folders: <strong>DataPreprocessing</strong>, <strong>DataVisualization</strong>, <strong>Figure</strong>, <strong>GetDescriptors</strong>, <strong>MERGE_POS_NEG</strong>.
 
 * <strong>DataPreprocessing</strong>: contains data wrangling codes
 
@@ -8,3 +8,6 @@ There are three folders: <strong>DataPreprocessing</strong>, <strong>DataVisuali
 
 * <strong>Figure</strong>: contains plots created by running visualization codes
 
+* <strong>GetDescriptors</strong>: contains codes of getting descriptors based on SMILES
+
+* <strong>MERGE_POS_NEG</strong>: contains codes of processing metabolites data in positive and negative modes
